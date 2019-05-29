cc9cfa9fd6985a643737816d43d4b6af90cf26f0
FreshMan <qinbocai@sina.cn>
2019-05-29 23:18:42 +0800

    添加图表显示；

---
+++ WeChatCms/Content/Scripts/CostNote/coststatistical.js
@@ -0,0 +1,206 @@
+﻿//根目录
+var hidRootUrl = $("#hidRootNode").val();
+/*
+ * 初始化
+ */
+function initCostChannelInfo() {
+    //加载
+    $(".loading-container").removeClass("loading-inactive");
+    $.ajax(hidRootUrl + "/CostNote/InitCostChannelModel", {
+        type: "POST",
+        success: function (result) {
+            if (result && result.ResultCode == 0) {
+                parent.layer.msg("操作成功!");
+                Search(1);
+            } else
+                parent.layer.msg(result.Message);
+            //取消加载
+            $(".loading-container").addClass("loading-inactive");
+        },
+        error: function () {
+            parent.layer.msg("系统异常");
+            //取消加载
+            $(".loading-container").addClass("loading-inactive");
+        }
+    });
+}
+
+
+var costtypenumlist = [];
+var searchVm = new Vue({
+    el: '#form1',
+    data: {
+        costcontentmodel: {
+            starttime: '',
+            endtime: '',
+            costtype: ''
+        },
+        staticcosttypenum: costtypenumlist
+    }
+});
+
+function drawCanPayLine() {
+    var dom = document.getElementById("costPay");
+    var myChart = echarts.init(dom);
+    var app = {};
+    option = null;
+    app.title = '堆叠条形图';
+
+    option = {
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: { // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+            }
+        },
+        legend: {
+            data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+        },
+        grid: {
+            left: '0%',
+            right: '0%',
+            bottom: '0%',
+            containLabel: true
+        },
+        xAxis: {
+            type: 'value'
+        },
+        yAxis: {
+            type: 'category',
+            data: ['周一']
+        },
+        series: [{
+            name: '直接访问',
+            type: 'bar',
+            stack: '总量',
+            label: {
+                normal: {
+                    show: true,
+                    position: 'insideRight'
+                }
+            },
+            data: [320]
+        }, {
+            name: '邮件营销',
+            type: 'bar',
+            stack: '总量',
+            label: {
+                normal: {
+                    show: true,
+                    position: 'insideRight'
+                }
+            },
+            data: [120]
+        }, {
+            name: '联盟广告',
+            type: 'bar',
+            stack: '总量',
+            label: {
+                normal: {
+                    show: true,
+                    position: 'insideRight'
+                }
+            },
+            data: [220]
+        }, {
+            name: '视频广告',
+            type: 'bar',
+            stack: '总量',
+            label: {
+                normal: {
+                    show: true,
+                    position: 'insideRight'
+                }
+            },
+            data: [150]
+        }, {
+            name: '搜索引擎',
+            type: 'bar',
+            stack: '总量',
+            label: {
+                normal: {
+                    show: true,
+                    position: 'insideRight'
+                }
+            },
+            data: [820]
+        }]
+    };;
+    if (option && typeof option === "object") {
+        myChart.setOption(option, true);
+    }
+}
+
+function drawCostType() {
+    var dom = document.getElementById("costMonth");
+    var myChart = echarts.init(dom);
+    var app = {};
+    option = null;
+    option = {
+        title: {
+            text: '深圳月最低生活费组成（单位:元）',
+            subtext: 'From ExcelHome',
+            sublink: 'http://e.weibo.com/1341556070/AjQH99che'
+        },
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            },
+            formatter: function (params) {
+                var tar = params[1];
+                return tar.name + '<br/>' + tar.seriesName + ' : ' + tar.value;
+            }
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: {
+            type: 'category',
+            splitLine: { show: false },
+            data: ['总费用', '房租', '水电费', '交通费', '伙食费', '日用品数']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {
+                name: '辅助',
+                type: 'bar',
+                stack: '总量',
+                itemStyle: {
+                    normal: {
+                        barBorderColor: 'rgba(0,0,0,0)',
+                        color: 'rgba(0,0,0,0)'
+                    },
+                    emphasis: {
+                        barBorderColor: 'rgba(0,0,0,0)',
+                        color: 'rgba(0,0,0,0)'
+                    }
+                },
+                data: [0, 1700, 1400, 1200, 300, 0]
+            },
+            {
+                name: '生活费',
+                type: 'bar',
+                stack: '总量',
+                label: {
+                    normal: {
+                        show: true,
+                        position: 'inside'
+                    }
+                },
+                data: [2900, 1200, 300, 200, 900, 300]
+            }
+        ]
+    };
+    ;
+    if (option && typeof option === "object") {
+        myChart.setOption(option, true);
+    }
+}
+drawCanPayLine();
+drawCostType();
