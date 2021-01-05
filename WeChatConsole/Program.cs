aff3cd778bf725a2c3f22d8699d678fbff7fa7f3
FreshMan <qinbocai@sina.cn>
2021-01-05 16:01:35 +0800

    添加文件；

---
+++ WeChatConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WeChatConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
+        }
+    }
+}
