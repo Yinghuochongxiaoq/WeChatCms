71d12bf9fff025a6ec9e21eee4eda5e7250845ab
FreshMan <qinbocai@sina.cn>
2021-01-15 10:58:33 +0800

    添加功能列表接口返回；

---
+++ WeChatNoteCostApi/Controllers/ConfigFunctionController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using WeChatCmsCommon.EnumBusiness;
+using WeChatModel;
+
+namespace WeChatNoteCostApi.Controllers
+{
+    public class ConfigFunctionController : ApiControllerBase
+    {
+        /// <summary>
+        /// 绑定账户
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [AllowAnonymous]
+        public ResponseBaseModel<dynamic> MyConfigInfo()
+        {
+            return new ResponseBaseModel<dynamic>
+            {
+                ResultCode = ResponceCodeEnum.Success,
+                Message = "",
+                Data = new List<dynamic>{
+                new {
+                    title = "账户设置",
+                    link = "/pages/costchannel/costchannel"
+                },
+                    new {
+                        title = "消费类型设置",
+                        link = "/pages/costtype/costtype"
+                    },
+                    new {
+                        title = "绑定家庭成员",
+                        link = "/pages/bindfamily/index"
+                    },
+                    new {
+                        title = "打卡记录",
+                        link = "/pages/calendar/calendar"
+                    },
+                    //new {
+                    //    title = "轻松一刻",
+                    //    link = "/pages/2048/2048"
+                    //},
+                    new {
+                        title = "关于我们",
+                        link = "./about"
+                    }
+                }
+            };
+        }
+    }
+}
